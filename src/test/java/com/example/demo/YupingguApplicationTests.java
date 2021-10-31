9bf53877a70410dcbc6fc049dd14928c348caaaf
chenchen <2391848749@.com>
2021-10-31 15:58:20 +0800

    '修改'

--- src/test/java/com/example/demo/YupingguApplicationTests.java
+++ src/test/java/com/example/demo/YupingguApplicationTests.java
@@ -15,9 +15,6 @@ import java.util.Date;
 @RunWith(SpringRunner.class)
 class YupingguApplicationTests {
 
-    @Autowired
-    private ProjectMsgService projectMsgService;
-
     @Test
     void contextLoads() {
 
@@ -25,30 +22,6 @@ class YupingguApplicationTests {
 
     @Test
     public void test01(){
-
-        ProjectMsg projectMsg = new ProjectMsg();
-
-        projectMsg.setProjectName("项目1");
-        projectMsg.setBudget("1");
-        projectMsg.setBuildingGoal("1");
-        projectMsg.setBusinessLogic("1");
-        projectMsg.setBusinessPlan("1");
-        projectMsg.setDemandAnalysis("1");
-        projectMsg.setDepartment("bumen");
-        projectMsg.setEnviromentInfluence("1");
-        projectMsg.setImplementation("1");
-        projectMsg.setInvestment(11d);
-        projectMsg.setMoney(11d);
-        projectMsg.setPerformance(1);
-        projectMsg.setState(1);
-        projectMsg.setProgramme("1");
-        projectMsg.setScheme("1");
-        projectMsg.setStatusAnalysis("1");
-        projectMsg.setCreateTime(new Date());
-        projectMsg.setCreatorId(1L);
-        projectMsg.setProjectPerformence("1");
-
-        projectMsgService.addProjectMsg(projectMsg);
 
     }
 
